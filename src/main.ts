5904a48f7ae6630c108c0d50d04e79f0b99e643e
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2023-05-23 11:29:20 +0300

    remove adminjs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { setupAdminPanel } from './admin-panel/admin-panel.plugin.js';
+// import { setupAdminPanel } from './admin-panel/admin-panel.plugin.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await setupAdminPanel(app);
+  // await setupAdminPanel(app);
 
   await app.listen(3000);
 }
